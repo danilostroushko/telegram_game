d950c91960f2fbeb482e9b5a47e881b51aa0ddb9
danilostroushko <danil.ostroushko@gmail.com>
2024-06-17 12:56:38 +0300

    update + input test

--- server/server.js
+++ server/server.js
@@ -11,22 +11,7 @@ const HOOK_PATH = process.env.HOOK_PATH || "hook";
 
 
 const rules = `
-Each player is dealt 26 cards.
-
-Players alternately discard one card from their hand.
-
-When some of the following combinations occur players race to slap the discard pile:
- * Double (e.g. 2-2)
- * Marriage (e.g. K-Q)
- * Sandwich (e.g. 2-5-2)
- * Divorce (e.g. Q-10-K)
- * Three in a Row (e.g. K-1-2, 3-4-5)
-
-The player who slaps first takes all the cards from the pile.
-
-If someone slaps when there is no valid combination, the other player takes all the cards.
-
-The goal of the game is to have all 52 cards in your hand.
+Test bot.
 `
 
 
