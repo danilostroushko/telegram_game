5ac6405a3af5ec738ff9b7490866128eefab3d4d
danilostroushko <danil.ostroushko@gmail.com>
2024-06-19 12:15:21 +0300

    cors 4

--- server/server.js
+++ server/server.js
@@ -42,6 +42,10 @@ app.use((req, res, next) => {
         return res.sendStatus(301);
     }
 
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
     next();
 })
 
