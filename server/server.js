cf66ab4fcaf8d0fe94f127e15c1c39f6ca79bff8
danilostroushko <danil.ostroushko@gmail.com>
2024-08-21 09:17:34 +0300

    fdgh

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
     const secret = req.get('X-Telegram-Bot-Api-Secret-Token');
 
     if (process.env.SECRET_TOKEN !== secret) {
-        //return res.sendStatus(301);
+        return res.sendStatus(301);
     }
 
 	res.header("Access-Control-Allow-Origin", "*");
